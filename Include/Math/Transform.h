5fb7c0e57a068b001c47fc70813c48f05ebd4ba4
liyanliang <liyanliang10778@navinfo.com>
2026-01-02 10:41:23 +0800

    modify: Include/Math/Matrix.h

--- Include/Math/Transform.h
+++ Include/Math/Transform.h
@@ -575,15 +575,15 @@ public:
     {
         TMatrix<T> Result = TMatrix<T>::MakeFromQuat(Rotation);
 
-        // Apply scale to rotation rows (UE5 convention)
+        // Apply scale to rotation columns
         Result.M[0][0] *= Scale3D.X; Result.M[0][1] *= Scale3D.X; Result.M[0][2] *= Scale3D.X;
         Result.M[1][0] *= Scale3D.Y; Result.M[1][1] *= Scale3D.Y; Result.M[1][2] *= Scale3D.Y;
         Result.M[2][0] *= Scale3D.Z; Result.M[2][1] *= Scale3D.Z; Result.M[2][2] *= Scale3D.Z;
 
-        // Apply translation in row 3 (UE5 convention)
-        Result.M[3][0] = Translation.X;
-        Result.M[3][1] = Translation.Y;
-        Result.M[3][2] = Translation.Z;
+        // Apply translation in column 3 (column-vector convention for shader M * v)
+        Result.M[0][3] = Translation.X;
+        Result.M[1][3] = Translation.Y;
+        Result.M[2][3] = Translation.Z;
 
         return Result;
     }
@@ -593,10 +593,10 @@ public:
     {
         TMatrix<T> Result = TMatrix<T>::MakeFromQuat(Rotation);
 
-        // Translation in row 3 (UE5 convention)
-        Result.M[3][0] = Translation.X;
-        Result.M[3][1] = Translation.Y;
-        Result.M[3][2] = Translation.Z;
+        // Translation in column 3 (column-vector convention for shader M * v)
+        Result.M[0][3] = Translation.X;
+        Result.M[1][3] = Translation.Y;
+        Result.M[2][3] = Translation.Z;
 
         return Result;
     }
